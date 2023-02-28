ec1c32bf4ffdd3fd0d756b2c8d6bdff228669cd4
Sportek <gablandry31@gmail.com>
2023-02-28 13:21:56 -0500

    librairie fonctionnelle

--- project/main.cpp
+++ project/main.cpp
@@ -1,19 +1,12 @@
 #define F_CPU 8000000UL
-#include "Logger/Logger.hpp"
+#include <Logger/Logger.hpp>
 #include <util/delay.h>
-#include "ExternInterrupt/ExternInterrupt.hpp"
 
 int main() {
     Logger::init();
     _delay_ms(5000);
-    Logger::log(Priority::INFO, "Le programme est lancé.");
-    ExternInterrupt::init(InterruptType::ANY);
-    uint8_t value = 0;
-    while(true){
-        if(ExternInterrupt::getInterruptCount() > value) {
-            value = ExternInterrupt::getInterruptCount();
-            Logger::log(Priority::INFO, "Interruption détectée");
-            Logger::log(Priority::INFO, ExternInterrupt::convertClickTypeToString(ExternInterrupt::getClickType()));
-        }
-    }
+    // Log a possibilité d'avoir un 3ième argument
+    // qui permet d'éviter de faire un saut à la ligne
+    // de façon automatique.
+    Logger::log(Priority::INFO, "Coucou les potes");
 }
