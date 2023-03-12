7db02e3267f3ca281e54bc8ec766b996fa765377
Sportek <gablandry31@gmail.com>
2023-03-11 23:07:15 -0500

    Retrait d'un chiffre magique

--- library/Timer/TimerManager.cpp
+++ library/Timer/TimerManager.cpp
@@ -5,6 +5,7 @@ volatile bool isTimerDone = true;
 bool TimerManager::isAlreadyChecked = false;
 
 void TimerManager::runTimer(uint16_t duration) {
+    static float TENTH_OF_A_SECOND = 781.25;
     if(isTimerDone == false) {
         Logger::init();
         Logger::log(Priority::ERROR, "Un timer a essayé d'être lancé, alors qu'un autre n'est pas terminé.");
@@ -14,7 +15,7 @@ void TimerManager::runTimer(uint16_t duration) {
     // mode CTC du timer 1 avec horloge divisée par 1024
     // interruption après la durée spécifiée
     TCNT1 = 0; // Ici on met le début du compteur.
-    OCR1A = duration * 781.25; // En gros ici on met le nombre maximum à atteindre
+    OCR1A = duration * TENTH_OF_A_SECOND; // En gros ici on met le nombre maximum à atteindre
     TCCR1A = (1 << WGM12);
     TCCR1B = (1 << CS10) | (1 << CS12); // Explication page 629 de la documentation, mais en gros ça met le bit 2 et le bit 0 à 1, pour avoir le tout /1024 (normalement).
     TCCR1C = 0;
