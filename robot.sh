b2d93843b53571aad65205ef64a80906e70f328f
Sportek <gablandry31@gmail.com>
2023-03-13 11:30:37 -0400

    Modification supplémentaire du script d'exécution

--- robot.sh
+++ robot.sh
@@ -6,20 +6,20 @@ if [ "$1" = "debug" ]; then
     make build
     cd ../project
     make debug
-elif [ "$1" = "clean" ]; then
-    cd library
-    make clean
-    cd ../project
-    make clean
-    echo -e "\e[32mSuppression de l'ensemble des fichiers inutiles.\e[0m"
 elif [ "$1" = "install" ]; then
     echo "Exécution du script en mode installation."
     cd library
     make build
     cd ../project
     make install
+elif [ "$1" = "clean" ]; then
+    cd library
+    make clean
+    cd ../project
+    make clean
+    echo -e "\e[32mSuppression de l'ensemble des fichiers inutiles.\e[0m"
 else 
-    echo -e "\e[31mArgument invalide : utilisez 'debug' ou 'clean'.\e[0m"
+    echo -e "\e[31mArgument invalide : utilisez 'debug', 'install' ou 'clean'.\e[0m"
     exit 1
 fi
 
