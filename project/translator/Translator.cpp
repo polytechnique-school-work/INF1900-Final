3754ea1257b426eab17c30db20e6f8b8f6e79f31
Sportek <gablandry31@gmail.com>
2023-03-13 22:07:32 -0400

    ajout de la base principale

--- project/translator/Translator.cpp
+++ project/translator/Translator.cpp
@@ -1,13 +1,69 @@
 #include "Translator.hpp"
 
-void Translator::read() {
-    
-}
-
 void Translator::translate() {
-    
+    // Lecture des valeurs
+    Memoire24CXXX memory;
+    uint8_t values[2];
+    memory.lecture(0, &values[0]);
+    memory.lecture(1, &values[1]);
+
+    this->nInstructions = ((uint16_t)values[1] << 8) | values[0];
+    this->instructions[this->nInstructions];
+    memory.lecture(2, instructions, this->nInstructions);
+
+    // Exécution des entrées
 }
 
-void Translator::execute() {
-    
+void Translator::execute(uint8_t instruction, uint8_t arg) {
+
+    Mnemonic mnemonic = static_cast<Mnemonic>(instruction);
+    switch (mnemonic) {
+        case Mnemonic::DBT:
+            /* code */
+            break;
+        case Mnemonic::ATT:
+            /* code */
+            break;
+        case Mnemonic::DAL:
+            /* code */
+            break;
+        case Mnemonic::DET:
+            /* code */
+            break;
+        case Mnemonic::SGO:
+            /* code */
+            break;
+        case Mnemonic::SAR:
+            /* code */
+            break;
+        case Mnemonic::MAR1:
+            /* code */
+            break;
+        case Mnemonic::MAR2:
+            /* code */
+            break;
+        case Mnemonic::MAV:
+            /* code */
+            break;
+        case Mnemonic::MRE:
+            /* code */
+            break;
+        case Mnemonic::TRD:
+            /* code */
+            break;
+        case Mnemonic::TRG:
+            /* code */
+            break;
+        case Mnemonic::DBC:
+            /* code */
+            break;
+        case Mnemonic::FBC:
+            /* code */
+            break;
+        case Mnemonic::FIN:
+            /* code */
+            return;
+        default:
+            break;
+    }
 }
--- project/translator/Translator.hpp
+++ project/translator/Translator.hpp
@@ -1,31 +1,30 @@
 #pragma once
-#include <avr/io.h> 
+#include <Memory/memoire_24.h>
+#include <avr/io.h>
 enum class Mnemonic {
-    DBT = 0b00000001, // Début
-    ATT = 0b00000010, // Attendre
-    DAL = 0b01000100, // Allumer la del
-    DET = 0b01000101, // Éteindre la del
-    SGO = 0b01001000, // Jouer une sonorité
-    SAR = 0b00001001, // Arrêter de jouer la sonorité
-    MAR1 = 0b01100000,// Arrêter les moteurs 1
-    MAR2 = 0b01100001,// Arrêter les moteurs 2
-    MAV = 0b01100010, // Avancer
-    MRE = 0b01100011, // Reculer
-    TRD = 0b01100100, // Tourner à droite
-    TRG = 0b01100101, // Tourner à gauche
-    DBC = 0b11000001, // Début de boucle
-    FBC = 0b11000001, // Fin de la boucle
-    FIN = 0b11111111  // Fin
+    DBT  = 0b00000001, // Début
+    ATT  = 0b00000010, // Attendre
+    DAL  = 0b01000100, // Allumer la del
+    DET  = 0b01000101, // Éteindre la del
+    SGO  = 0b01001000, // Jouer une sonorité
+    SAR  = 0b00001001, // Arrêter de jouer la sonorité
+    MAR1 = 0b01100000, // Arrêter les moteurs 1
+    MAR2 = 0b01100001, // Arrêter les moteurs 2
+    MAV  = 0b01100010, // Avancer
+    MRE  = 0b01100011, // Reculer
+    TRD  = 0b01100100, // Tourner à droite
+    TRG  = 0b01100101, // Tourner à gauche
+    DBC  = 0b11000000, // Début de boucle
+    FBC  = 0b11000001, // Fin de la boucle
+    FIN  = 0b11111111, // Fin
 };
+
 class Translator {
 private:
-    uint8_t* instructions = nullptr;
-public:
-    void read();
-    void translate();
+    uint8_t *instructions = nullptr;
+    uint16_t nInstructions;
     void execute(uint8_t instruction, uint8_t arg);
 
-    ~Translator() {
-        delete[] instructions;
-    }
+public:
+    void translate();
 };
