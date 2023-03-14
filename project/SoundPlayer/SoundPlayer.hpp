7f0937ca6b23832b06e68e437218e67c050e49be
Marianna Prudhomme <marianna.prudhomme2@gmail.com>
2023-03-14 04:19:27 -0400

    ajouté TOUTES LES NOTES!

--- project/SoundPlayer/SoundPlayer.hpp
+++ project/SoundPlayer/SoundPlayer.hpp
@@ -3,7 +3,45 @@
 
 // Utilise le timer 0 afin de pouvoir utiliser la PWM des roues ET une minuterie en même temps.
 
-enum class Note { DO = 5, RE = 10 };
+int notes[36] = {
+4545,
+4290,
+4050,
+3822,
+3608,
+3405,
+3214,
+3034,
+2863,
+2703,
+2551,
+2408,
+2273,
+2145,
+2025,
+1911,
+1804,
+1703,
+1607,
+1517,
+1432,
+1351,
+1276,
+1204,
+1136,
+1073,
+1012,
+956,
+902,
+851,
+804,
+758,
+716,
+676,
+638,
+602,
+568
+}
 
 class SoundPlayer {
 private:
