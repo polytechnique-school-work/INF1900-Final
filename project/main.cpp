4d991e6d383f6e65b9ca65f0a22a6b2253877684
Sportek <gablandry31@gmail.com>
2023-03-24 14:44:20 -0400

    Mise en place du capteur avec le bon calcul

--- project/main.cpp
+++ project/main.cpp
@@ -8,7 +8,7 @@
  * */
 #define F_CPU 8000000UL
 
-#include "Translator/Translator.hpp"
+#include <Can/Can.hpp>
 #include <Light/LightManager.hpp>
 #include <Logger/Logger.hpp>
 #include <Memory/memoire_24.h>
@@ -21,12 +21,24 @@ static const uint16_t STARTUP_DELAY = 2000;
 
 void init() {
     _delay_ms(STARTUP_DELAY);
+    DDRA &= ~(1 << PORTA2);
     Logger::log(Priority::INFO, "Le programme est lancé.");
 }
 
 int main() {
     init();
-    Translator translator = Translator();
-    translator.translate(wheels, light);
+    // Translator translator = Translator();
+    // translator.translate(wheels, light);
+    // return 0;
+    while (true) {
+        Can can;
+        uint8_t value = uint8_t(can.lecture(2) >> 2);
+        DEBUG_PRINT((1200/(value-20))); // 600 = 80cm
+                                        // 10 = 10cm
+                                        // 15 = 15cm
+                                        // 1 d'espacement = 35/40.
+        _delay_ms(50);
+    }
+
     return 0;
 }
