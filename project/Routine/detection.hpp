7be40a65d199d7db07f8152eec857e5f780112a8
Marianna Prudhomme <marianna.prudhomme2@gmail.com>
2023-03-29 09:52:27 -0400

    added detection routine files

---
+++ project/Routine/detection.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#define F_CPU 8000000UL
+#include <ExternInterrupt/ExternInterrupt.hpp>
+#include <Light/LightManager.hpp>
+#include <Logger/Logger.hpp>
+#include <Wheel/WheelManager.hpp>
+#include <PWM/Pwm.hpp>
+#include <SoundPlayer/SoundPlayer.hpp>
+#include <avr/io.h> 
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+class RoutineDetection
+{
+public:
+    void executeRoutine()
+
+private:
+    
+}
