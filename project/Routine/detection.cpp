c701974a7ee624ca8c321c1bb0d97fc8a147beee
Marianna Prudhomme <marianna.prudhomme2@gmail.com>
2023-04-03 12:12:04 -0400

    for Maelys bestie

--- project/Routine/detection.cpp
+++ project/Routine/detection.cpp
@@ -1,44 +1,48 @@
 #include "detection.hpp"
 
-// // Dès le début du mode détection, le robot fait prendre la couleur ambrée à sa DEL. 
+// // Dès le début du mode détection, le robot fait prendre la couleur ambrée à sa DEL.
 
-// Il attend ensuite qu’on lui 
-// indique dans quelle orientation il a été placé. S’il pointe vers le haut de la table, il faudra appuyer sur le bouton 
-// Interrupt. S’il pointe vers la droite, sur le bouton blanc.
+// Il attend ensuite qu’on lui
+// indique dans quelle orientation il a été placé. S’il pointe vers le haut de la table, il faudra
+// appuyer sur le bouton Interrupt. S’il pointe vers la droite, sur le bouton blanc.
 
-// // Pour signifier qu’il a bien compris, le robot mettra sa DEL en vert lorsqu’on lui dit qu’il pointe vers 
-// le haut de la table, et en rouge pour la droite. La DEL garde cette couleur pendant 2 secondes puis s’éteint.
+// // Pour signifier qu’il a bien compris, le robot mettra sa DEL en vert lorsqu’on lui dit qu’il
+// pointe vers le haut de la table, et en rouge pour la droite. La DEL garde cette couleur pendant 2
+// secondes puis s’éteint.
 
-// // Le robot se met ensuite à la recherche du poteau. Dès qu’il le détecte, il se dirige vers celui-ci et 
-// s’arrête lorsqu’il se trouve à proximité (entre 10 et 15 cm). Évidemment, si le poteau se trouvait déjà à une 
-// distance située entre 10 et 15 cm (poteau voisin), le robot n’a pas à effectuer de déplacement. 
+// // Le robot se met ensuite à la recherche du poteau. Dès qu’il le détecte, il se dirige vers
+// celui-ci et s’arrête lorsqu’il se trouve à proximité (entre 10 et 15 cm). Évidemment, si le
+// poteau se trouvait déjà à une distance située entre 10 et 15 cm (poteau voisin), le robot n’a pas
+// à effectuer de déplacement.
 
-//Le robot émet ensuite une série de trois sons aigus. Les sons et les pauses qui les séparent sont tous d’une durée de 
-// 300 millisecondes. 
+// Le robot émet ensuite une série de trois sons aigus. Les sons et les pauses qui les séparent sont
+// tous d’une durée de
+//  300 millisecondes.
 
-//Finalement, le robot fait clignoter sa DEL en couleur ambrée à 2 Hz et se met en attente.
+// Finalement, le robot fait clignoter sa DEL en couleur ambrée à 2 Hz et se met en attente.
 
-// // L’ évaluateur prend le poteau et le place sur un nouveau point. Il place ensuite le centre de rotation du 
-// robot sur le point qui vient d’être libéré du poteau. Il oriente le robot vers le haut de la table. 
+// // L’ évaluateur prend le poteau et le place sur un nouveau point. Il place ensuite le centre de
+// rotation du robot sur le point qui vient d’être libéré du poteau. Il oriente le robot vers le
+// haut de la table.
 
-//L’évaluateur appuie alors sur le bouton Interrupt pour indiquer au robot d’arrêter de faire clignoter sa DEL et de chercher le 
-// nouvel emplacement du poteau.
+// L’évaluateur appuie alors sur le bouton Interrupt pour indiquer au robot d’arrêter de faire
+// clignoter sa DEL et de chercher le
+//  nouvel emplacement du poteau.
 
-// // De 5 à 8 poteaux seront ainsi présentés au robot pour être détectés. Lorsqu’il aura jugé que c’est suffisant, 
-// l’évaluateur gardera le poteau dans ses mains avant d’appuyer sur le bouton Interrupt. 
+// // De 5 à 8 poteaux seront ainsi présentés au robot pour être détectés. Lorsqu’il aura jugé que
+// c’est suffisant, l’évaluateur gardera le poteau dans ses mains avant d’appuyer sur le bouton
+// Interrupt.
 
-//Ensuite, dans sa procédure 
-// de détection de poteau, le robot devra pouvoir déterminer qu’il n’y a plus de poteau sur la table pouvant être 
-// détectés. Dès qu’il arrive à cette conclusion, le robot arrête de bouger et émet un son grave pendant 2 secondes. 
-// Il fait ensuite clignoter sa DEL en rouge à 2 Hz.
+// Ensuite, dans sa procédure
+//  de détection de poteau, le robot devra pouvoir déterminer qu’il n’y a plus de poteau sur la
+//  table pouvant être détectés. Dès qu’il arrive à cette conclusion, le robot arrête de bouger et
+//  émet un son grave pendant 2 secondes. Il fait ensuite clignoter sa DEL en rouge à 2 Hz.
 
-void RoutineDetection::loopSound()
-{
+void RoutineDetection::loopSound() {
     SoundPlayer sp;
     sp.init();
 
-    for (int i = 0; i<3; i++)
-    {
+    for (int i = 0; i < 3; i++) {
         sp.playSound(45);
         _delay_ms(300);
         sp.reset();
@@ -46,8 +50,7 @@ void RoutineDetection::loopSound()
     }
 }
 
-void RoutineDetection::sonGrave()
-{
+void RoutineDetection::sonGrave() {
     SoundPlayer sp;
     sp.init();
 
@@ -56,12 +59,10 @@ void RoutineDetection::sonGrave()
     sp.reset();
 }
 
-void RoutineDetection::flashRed()
-{
-    LightManager lm(&DDRA, &PORTA, PORTA0, PORTA1); 
+void RoutineDetection::flashRed() {
+    LightManager lm(&DDRA, &PORTA, PORTA0, PORTA1);
 
-    while(true)
-    {
+    while (true) {
         lm.setLight(Color::AMBER);
         _delay_ms(250);
         lm.setLight(Color::OFF);
@@ -69,26 +70,22 @@ void RoutineDetection::flashRed()
     }
 }
 
-void RoutineDetection::flashAmber()
-{
+void RoutineDetection::flashAmber() {
     LightManager lm(&DDRA, &PORTA, PORTA0, PORTA1);
 
-    while(true)
-    {
-        for (int k = 0; k <12; k ++) {
-        lm.setLight(Color::AMBER);
+    while (true) {
+        for (int k = 0; k < 12; k++) {
+            lm.setLight(Color::AMBER);
         }
         lm.setLight(Color::OFF);
         _delay_ms(250);
     }
 }
 
-void RoutineDetection::executeRoutine()
-{
+void RoutineDetection::executeRoutine() {
     RoutineSteps routineSteps;
 
-    switch(routineSteps)
-    {
+    switch (routineSteps) {
         case RoutineSteps::START:
             // 1.Light Amber
             while (true) {
@@ -109,33 +106,34 @@ void RoutineDetection::executeRoutine()
                 }
             }
 
-            //2.Checker si l'orientation est haut ou droite
-
-
         case RoutineSteps::INT_CLICKED:
-            //orienté vers le haut
-            lm.setLight(Color::GREEN);
+            // orienté vers le haut
+            while (true) {
+                lm.setLight(Color::GREEN);
+            }
 
         case RoutineSteps::WHITE_CLICKED:
-            //orienté vers la droite
-            lm.setLight(Color::RED);
+            // orienté vers la droite
+            while (true) {
+                lm.setLight(Color::RED);
+            }
 
         case RoutineSteps::FIND_STICK:
-            //fonction de Gab
+            // fonction de Gab
 
         case RoutineSteps::FOUND_STICK:
-            //5. 3 sons aigus: son (300 ms), pause(300ms) 3x
+            // 5. 3 sons aigus: son (300 ms), pause(300ms) 3x
             loopSound();
-            //se dirige vers
+            // se dirige vers
 
         case RoutineSteps::WAIT:
-            //6.clignoter led ambrée à 2Hz -> 2 tours par seconde
+            // 6.clignoter led ambrée à 2Hz -> 2 tours par seconde
             flashAmber();
-            //Jusqu'à temps qu'on pèse sur interrupt
+            // Jusqu'à temps qu'on pèse sur interrupt
 
         case RoutineSteps::NO_STICK:
             sonGrave();
             flashRed();
-            //fin du switch case
+            // fin du switch case
     }
 }
