fa4b105c25d8f172c07fe2f0e4a46a9244031c22
Sportek <gablandry31@gmail.com>
2023-04-12 14:11:53 -0400

    Modif sensor

--- library/Sensor/Sensor.cpp
+++ library/Sensor/Sensor.cpp
@@ -1,8 +1,9 @@
 #include "Sensor.hpp"
 #include "Logger/Logger.hpp"
+#include "math.h"
 
 uint16_t Sensor::readValue() {
-    DEBUG_PRINT("Hello");
     uint8_t value = uint8_t(this->can.lecture(2) >> 2);
-    return (1200 / (value - 20));
+    return 1317.6 * powf(value, -0.874); // 20.094 * powf(value, -0.91); // 316.72 * powf(value,
+                                         // -0.545); //(1200 / (value - 20));
 }
