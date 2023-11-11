c8bd4bbae3d076152f64222b50ef5d6f906d3329
Gabriel <gablandry31@gmail.com>
2023-11-11 13:29:05 -0500

    Revert "Terminé, dodo time, fuck le robot, au revoir"
    This reverts commit 24da245d4b3f6613e312aa832dad653cabfa2628.

--- project/Constantes.hpp
+++ project/Constantes.hpp
@@ -3,7 +3,7 @@
 static const uint16_t STARTUP_DELAY                  = 2000;
 static const uint32_t TURN_DURATION                  = 90;
 static const uint8_t MAX_TURN                        = 8;
-static const uint16_t MINIMAL_DISTANCE_ACCEPTED      = 160; // 140
+static const uint16_t MINIMAL_DISTANCE_ACCEPTED      = 147; // 140
 static const uint16_t FIRST_DISTANCE                 = 220; // 210, 220
 static const uint16_t SECOND_DISTANCE                = 250; // 250 // 225
 static const uint8_t ROBOT_SPEED                     = 50;
